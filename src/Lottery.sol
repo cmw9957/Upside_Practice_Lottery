14348db6769f4935392749181ef6afd3d58d9002
cmw9957 <cmw9957@gmail.com>
2025-02-12 21:16:03 +0900

    feat : buy

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -11,12 +11,21 @@ interface ILottery {
 }
 
 contract Lottery is ILottery {
+
+    mapping(address => uint) lotteryList;
+    uint public vault_balance;
+
     function buy(uint lotteryNum) payable external {
-        
+        require(block.timestamp < block.timestamp + 24, "Too late to buy.");
+        require(msg.value == 0.1 ether, "Insufficient funds.");
+        require(lotteryList[msg.sender] != lotteryNum, "Already exists.");
+
+        vault_balance += msg.value;
+        lotteryList[msg.sender] = lotteryNum;
     }
 
     function draw() external {
-
+        
     }
     
     function claim() external {
