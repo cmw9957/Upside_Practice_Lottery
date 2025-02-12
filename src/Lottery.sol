12a14d220e4adef4cc35259a853882aea6681cce
cmw9957 <cmw9957@gmail.com>
2025-02-12 21:29:49 +0900

    fix : buy

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -3,36 +3,39 @@ pragma solidity ^0.8.13;
 
 import "forge-std/console.sol";
 
-interface ILottery {
-    function buy(uint lotteryNum) payable external;
-    function draw() external;
-    function claim() external;
-    function winningNumber() external returns (uint16);
-}
+// interface ILottery {
+//     function buy(uint lotteryNum) payable external;
+//     function draw() external;
+//     function claim() external;
+//     function winningNumber() public returns (uint16);
+// }
 
-contract Lottery is ILottery {
+contract Lottery {
 
     mapping(address => uint) lotteryList;
+    mapping(address => bool) buyList;
+    
     uint public vault_balance;
 
     function buy(uint lotteryNum) payable external {
         require(block.timestamp < block.timestamp + 24, "Too late to buy.");
         require(msg.value == 0.1 ether, "Insufficient funds.");
-        require(lotteryList[msg.sender] != lotteryNum, "Already exists.");
+        require(buyList[msg.sender] != true, "Already exists.");
 
         vault_balance += msg.value;
         lotteryList[msg.sender] = lotteryNum;
+        buyList[msg.sender] = true;
     }
 
     function draw() external {
-        
+
     }
     
     function claim() external {
 
     }
 
-    function winningNumber() external returns (uint16){
+    function winningNumber() public returns (uint16){
         return 0;
     }
 }
